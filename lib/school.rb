9736076405a55a2d77edab275f508b8602cf1156
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-04 18:05:15 -0400

    committing the lab adding the class School with all its instance variable and methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_reader 
+    attr_writer
+    attr_accessor :roster
+
+    def initialize (name)
+        @name = name
+        @roster ={}
+    end
+    def add_student (stud_name, grade_nm)
+        if @roster[grade_nm]
+            @roster[grade_nm].push(stud_name)
+        else
+            @roster[grade_nm] = [stud_name]
+        end
+    end
+    def grade (grade_nm)
+        @roster[grade_nm]
+    end
+    def sort
+        @roster.each do |grd, students|
+            students.sort!
+        end
+    end
+end
