1614a8e017bae111128a756eede5af966f3e3517
AmSmo <adam.smolenski@gmail.com>
2020-08-04 11:30:56 -0400

    sorted

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+class School
+    attr_accessor :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade) 
+            roster[grade] << name
+        else
+        roster[grade] = [name]
+        end
+        
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        
+       roster.each { |grades, students| roster[grades] = students.sort}
+       
+    end
+end
